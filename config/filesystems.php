aa4df0078695b7811133264e6d9cbfa5cc2d09f1
neoscar <neoscar10@gmail.com>
2025-11-13 20:58:36 +0100

    updated config/filesystem to correctly point files on shared hosting

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,11 +37,10 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
+        'driver' => 'local',
+        'root' => public_path('storage'),
+        'url' => env('APP_URL').'/storage',
+        'visibility' => 'public',
         ],
 
         's3' => [
